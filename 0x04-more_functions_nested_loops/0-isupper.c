315e245d3712682ccafb4b28e3c62e9d9008d284
Odjidja <tfelixodjidja@gmail.com>
2021-06-25 09:21:51 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,11 @@
  *
  * Return: 1 if character is uppercase, 0 otherwise.
  */
-
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-return (1);
- printf("%c: %d\n",c ,_isupper(c));
-else
-return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	else
+		return (0);
 }
