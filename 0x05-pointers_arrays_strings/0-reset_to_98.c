41f9df538e1c12ddb31191c64dd7f65125133867
Odjidja <tfelixodjidja@gmail.com>
2021-06-29 06:37:48 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates
+ * it to 98
+ * @n: the pointer to update variable
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
