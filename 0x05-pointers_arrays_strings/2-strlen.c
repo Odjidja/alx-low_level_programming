56380902aa19c46bef3160786ac457584341e4e4
Odjidja <tfelixodjidja@gmail.com>
2021-06-29 06:55:15 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+
+#include "holberton.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: string to be input
+ *
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+int i;
+
+i = 0;
+
+while (s[i] != '\0')
+{
+i++;
+}
+
+return (i);
+}
